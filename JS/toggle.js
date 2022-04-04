85030c8ee96ff3f6a4a55202bf607b7d54f32153
Hitesh Verma <hitesh.verma2711@gmail.com>
2022-04-04 23:42:28 +0530

    added hamburger and submenu component

--- JS/toggle.js
+++ JS/toggle.js
@@ -1,26 +1,32 @@
-const sidebar = document.querySelector(".sidebar")
-document.querySelector(".menu-icon").addEventListener("click", ()=>{
-    if (sidebar.style.display === "none") {
-        sidebar.style.display = "block";
-      } else {
-        sidebar.style.display = "none";
-      }
-})
+const sidebar = document.querySelector(".sidebar");
+document.querySelector(".menu-icon").addEventListener("click", () => {
+  if (sidebar.style.display === "none") {
+    sidebar.style.display = "block";
+  } else {
+    sidebar.style.display = "none";
+  }
+});
+const sidebarHam = document.querySelector("#sidebar-hamburger");
+document.querySelector(".menu-icon-hamburger").addEventListener("click", () => {
+  if (sidebarHam.style.display === "none") {
+    sidebarHam.style.display = "block";
+  } else {
+    sidebarHam.style.display = "none";
+  }
+});
 const snackbar = document.querySelector("#snackbar");
-document.querySelector("#show-snackbar-btn").addEventListener("click", ()=>{
+document.querySelector("#show-snackbar-btn").addEventListener("click", () => {
   if (snackbar.style.display === "none") {
     snackbar.style.display = "block";
   } else {
     snackbar.style.display = "none";
   }
-})
+});
 const floatingActionBtn = document.querySelector(".floating-button");
-document.querySelector(".show-floating-btn").addEventListener("click", ()=>{
+document.querySelector(".show-floating-btn").addEventListener("click", () => {
   if (floatingActionBtn.style.display === "none") {
     floatingActionBtn.style.display = "block";
   } else {
     floatingActionBtn.style.display = "none";
   }
-})
-
-
+});
